70b704edef7802087bc7e5db7e0ad09b6c5fb737
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-08 18:12:02 -0700

    Add ShipmentType

---
+++ schema/types/ShipmentType.js
@@ -0,0 +1,102 @@
+const graphql = require('graphql');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLNonNull
+} = graphql;
+
+const ScheduleType = require('./ScheduleType');
+const CompanyType = require('./CompanyType');
+const BookingRequestType = require('./BookingRequestType');
+const BookingConfirmationType = require('./BookingConfirmationType');
+const BillInstructionType = require('./BillInstructionType');
+const InvoiceType = require('./InvoiceType');
+
+
+const ShipmentType = new GraphQLObjectType({
+  name: 'ShipmentType',
+  fields: () => ({
+    schedule: {
+      type: new GraphQLNonNull(ScheduleType)
+    },
+    bookedBy: {
+      type: new GraphQLNonNull(CompanyType)
+    },
+    bookingRequest: {
+      type: new GraphQLNonNull(BookingRequestDataType)
+    },
+    billInstruction: {
+      type: new GraphQLNonNull(BillInstructionDataType)
+    },
+    invoice: {
+      type: new GraphQLNonNull(InvoiceDataType)
+    },
+    finance: {
+      type: new GraphQLNonNull(FinanceDataType)
+    },
+  })
+});
+
+const BookingRequestDataType = new GraphQLObjectType({
+  name: 'BookingRequestDataType',
+  fields: () => ({
+    form: {
+      type: new GraphQLNonNull(BookingRequestType)
+    },
+    confirmation: {
+      type: BookingConfirmationType
+    },
+    status: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+  })
+}); 
+
+const BillInstructionDataType = new GraphQLObjectType({
+  name: 'BillInstructionDataType',
+  fields: () => ({
+    form: {
+      type: new GraphQLNonNull(BillInstructionType)
+    },
+    pdf: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    status: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+  })
+}); 
+
+const InvoiceDataType = new GraphQLObjectType({
+  name: 'InvoiceDataType',
+  fields: () => ({
+    confirmation: {
+      type: new GraphQLNonNull(InvoiceType)
+    },
+    status: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+  })
+}); 
+
+const FinanceDataType = new GraphQLObjectType({
+  name: 'FinanceDataType',
+  fields: () => ({
+    cost: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    },
+    revenue: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    },
+    profit: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    },
+  })
+}); 
+
+module.exports = ShipmentType;
--- schema/types/index.js
+++ schema/types/index.js
@@ -2,6 +2,7 @@ const RouteType = require('./RouteType.js');
 const QuoteType = require('./QuoteType.js');
 const ScheduleType = require('./ScheduleType.js');
 const BookingRequestType = require('./BookingRequestType.js');
+const ShipmentType = require('./ShipmentType.js');
 
 const {
   QuoteInputType,
